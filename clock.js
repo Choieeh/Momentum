6a68a4bd28bad31f2fc1c82c4f168880118bfac5
Choieeh <chyh0182@naver.com>
2020-11-16 13:34:59 +0000

    last

--- clock.js
+++ clock.js
@@ -7,13 +7,14 @@ function getTime() {
 	const hours = date.getHours();
 	const seconds = date.getSeconds();
 	clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-	
+	//innerText는 문자열을 그대로 리턴하지만 innerHTML은 문자열을 html로 인식하여 리턴
 }
 // `(back) {문자열} 이랑 $ 이용하기 {데이터}
 
 function init() {
 	getTime();
 	setInterval(getTime, 1000);
+	//1초마다 새로고침
 }
 
 init();
